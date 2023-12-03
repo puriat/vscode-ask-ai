a39cd19f7aed084e1e3aa69a6a33a46f8857bb5c
pooria tazik <puria.tazik@gmail.com>
2023-12-03 15:23:15 +0100

    simple open ai question func

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -1,80 +1,94 @@
 import * as vscode from "vscode";
+import OpenAI from "openai";
 
 export class SidebarProvider implements vscode.WebviewViewProvider {
-    _view?: vscode.WebviewView;
-    _doc?: vscode.TextDocument;
+  _view?: vscode.WebviewView;
+  _doc?: vscode.TextDocument;
 
-    constructor(private readonly _extensionUri: vscode.Uri) { }
+  constructor(private readonly _extensionUri: vscode.Uri) {}
 
-    public resolveWebviewView(webviewView: vscode.WebviewView) {
-        this._view = webviewView;
+  public resolveWebviewView(webviewView: vscode.WebviewView) {
+    this._view = webviewView;
 
-        webviewView.webview.options = {
-            // Allow scripts in the webview
-            enableScripts: true,
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
 
-            localResourceRoots: [this._extensionUri],
-        };
+      localResourceRoots: [this._extensionUri],
+    };
 
-        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-        // Listen for messages from the Sidebar component and execute action
-        webviewView.webview.onDidReceiveMessage(async (data) => {
-            switch (data.type) {
-                case "onFetchText": {
-                    let editor = vscode.window.activeTextEditor;
+    // Listen for messages from the Sidebar component and execute action
+    webviewView.webview.onDidReceiveMessage(async (data) => {
+      switch (data.type) {
+        case "onFetchText": {
+          let editor = vscode.window.activeTextEditor;
 
-                    if (editor === undefined) {
-                        vscode.window.showErrorMessage('No active text editor');
-                        return;
-                    }
+          if (editor === undefined) {
+            vscode.window.showErrorMessage("No active text editor");
+            return;
+          }
 
-                    let text = editor.document.getText(editor.selection);
-                    // send message back to the sidebar component
-                    this._view?.webview.postMessage({ type: "onSelectedText", value: text });
-                    break;
-                }
-                case "onInfo": {
-                    if (!data.value) {
-                        return;
-                    }
-                    vscode.window.showInformationMessage(data.value);
-                    break;
-                }
-                case "onError": {
-                    if (!data.value) {
-                        return;
-                    }
-                    vscode.window.showErrorMessage(data.value);
-                    break;
-                }
-            }
-        });
+          let text = editor.document.getText(editor.selection);
+          // send message back to the sidebar component
+          this._view?.webview.postMessage({
+            type: "onSelectedText",
+            value: text,
+          });
+          break;
+        }
+        case "onAskAi": {
+          let { question, apiKey } = data.value;
 
-    }
+          if (question) {
+            this._view?.webview.postMessage({
+              type: "onAnswerText",
+              value: await askQuestion(question, apiKey),
+            });
+          }
+          break;
+        }
+        case "onInfo": {
+          if (!data.value) {
+            return;
+          }
+          vscode.window.showInformationMessage(data.value);
+          break;
+        }
+        case "onError": {
+          if (!data.value) {
+            return;
+          }
+          vscode.window.showErrorMessage(data.value);
+          break;
+        }
+      }
+    });
+  }
 
-    public revive(panel: vscode.WebviewView) {
-        this._view = panel;
-    }
+  public revive(panel: vscode.WebviewView) {
+    this._view = panel;
+  }
 
-    private _getHtmlForWebview(webview: vscode.Webview) {
-        const styleResetUri = webview.asWebviewUri(
-            vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
-        );
-        const styleVSCodeUri = webview.asWebviewUri(
-            vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
-        );
-        const scriptUri = webview.asWebviewUri(
-            vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.js")
-        );
-        const styleMainUri = webview.asWebviewUri(
-            vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.css")
-        );
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.js")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.css")
+    );
 
-        // Use a nonce to only allow a specific script to be run.
-        const nonce = getNonce();
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
 
-        return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -82,8 +96,7 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
 					Use a content security policy to only allow loading images from https or from our extension directory,
 					and only allow scripts that have a specific nonce.
         -->
-        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource
-            }; script-src 'nonce-${nonce}';">
+        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<link href="${styleResetUri}" rel="stylesheet">
 				<link href="${styleVSCodeUri}" rel="stylesheet">
@@ -97,14 +110,26 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
 				<script nonce="${nonce}" src="${scriptUri}"></script>
 			</body>
 			</html>`;
-    }
+  }
 }
 
 function getNonce() {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < 32; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function askQuestion(question: string, apiKey: string) {
+  const openai = new OpenAI({ apiKey });
+
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: question }],
+    model: "gpt-3.5-turbo",
+  });
+
+  return JSON.stringify(completion.choices[0].message.content);
 }
--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
     import { onMount } from "svelte";
 
-    let text = "";
+    let question = "";
+    let answer = "";
+    let apiKey = "";
 
     function fetchText() {
         // send message to the extension asking for the selected text
         tsvscode.postMessage({ type: "onFetchText", value: "" });
+    }
+
+    function askAi() {
+        // send message to the extension asking your question
+        tsvscode.postMessage({ type: "onAskAi", value: { question, apiKey } });
     }
 
     onMount(() => {
@@ -14,7 +21,11 @@
             const message = event.data;
             switch (message.type) {
                 case "onSelectedText": {
-                    text = message.value;
+                    question = message.value;
+                    break;
+                }
+                case "onAnswerText": {
+                    answer = message.value;
                     break;
                 }
             }
@@ -22,13 +33,20 @@
     });
 </script>
 
-<h1>Sidebar Panel</h1>
-<label for="text"><b>Selected text</b></label>
+<h1>Ask Bot</h1>
+
+<label for="text"><b>OPEN API KEY:</b></label>
+<input type="password" bind:value={apiKey} />
+
+<label for="text"><b>SHOOT IT</b></label>
 <textarea
     rows="15"
     id="text"
     style="resize: vertical;"
     minlength="30"
-    bind:value={text}
+    bind:value={question}
 />
 <button on:click={fetchText}>fetch text</button>
+<button on:click={askAi}>Ask</button>
+
+<span>{answer}</span>
